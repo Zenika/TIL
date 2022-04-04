ece8d8823a8d12c237f67993587f4d967ee070e8
Vincent LANDAIS <vincent.landais@zenika.com>
2022-04-04 12:00:31 +0200

    re testing deployment with vue

---
+++ client/src/components/ArticleCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <Card>
+    <template #title>
+      <div class="flex justify-content-center">
+        {{ post.title }}
+      </div>
+    </template>
+    <template #subtitle>{{ post.created_at }}</template>
+    <template #content>
+      <p class="text-justify">
+        {{ post.content }}
+      </p>
+      <p>By: {{ post.user.username }}</p>
+    </template>
+    <template #footer>
+      <div class="flex justify-content-center">
+        <Button
+          label="Check it out"
+          @click="$router.push(`/post/${post.id}`)"
+        />
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+const props = defineProps({
+  post: Object,
+});
+</script>
+
+<style scoped>
+</style>
