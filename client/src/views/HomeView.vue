e1969d4b68acde02edb329a2f2775251e6fa6690
Vincent LANDAIS <vincent.landais@zenika.com>
2022-04-07 09:57:03 +0200

    PostListItem component added to represent items from the list displayed in home

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,21 +4,9 @@
   <div v-else-if="result.post">
     <DataView :value="result.post" :layout="'list'">
       <template #list="slotProps">
-        <div class="col-12">
-          <div class="product-list-item">
-            <img
-              src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
-              :alt="slotProps.data.content"
-            />
-            <div class="product-list-detail">
-              <div class="product-name">
-                <a :href="slotProps.data.content" target="_blank" rel="noopener noreferrer">{{slotProps.data.content}}</a>
-              </div>
-              {{new Date(slotProps.data.created_at.replace(' ', 'T')).toDateString()}}
-            </div>
-          </div>
-        </div>
+        <PostListItem :post="slotProps.data"/>
       </template>
+      <template #empty>No records found.</template>
     </DataView>
   </div>
 </template>
@@ -26,8 +14,9 @@
 <script setup>
 import { useSubscription } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+import PostListItem from '../components/PostListItem.vue'
 
-const { result, loading, error } = useSubscription(gql`
+let { result, loading, error } = useSubscription(gql`
   subscription getPosts {
     post(order_by: {created_at: desc}) {
       content
@@ -35,43 +24,5 @@ const { result, loading, error } = useSubscription(gql`
     }
   }
 `);
+
 </script>
-
-<style lang="scss" scoped>
-.product-name {
-  font-size: 1.5rem;
-  font-weight: 700;
-}
-
-:deep(.product-list-item) {
-  display: flex;
-  align-items: center;
-  padding: 1rem;
-  width: 100%;
-
-  img {
-    width: 50px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    margin-right: 2rem;
-  }
-
-  .product-list-detail {
-    flex: 1 1 0;
-  }
-}
-
-@media screen and (max-width: 576px) {
-  .product-list-item {
-    flex-direction: column;
-    align-items: center;
-
-    img {
-      margin: 2rem 0;
-    }
-
-    .product-list-detail {
-      text-align: center;
-    }
-  }
-}
-</style>
