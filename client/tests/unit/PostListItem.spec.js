7c8f56c3f41c4c7a5978df956886d95914db8611
Vincent LANDAIS <vincent.landais@zenika.com>
2022-04-08 11:58:21 +0200

    fixed unit tests

--- client/tests/unit/PostListItem.spec.js
+++ client/tests/unit/PostListItem.spec.js
@@ -4,14 +4,17 @@ import PostListItem from '@/components/PostListItem.vue'
 describe('PostListItem.vue', () => {
   it('renders one post', () => {
     const post = {
-      content: "https://medium.com/@sanjiva.weerawarana/next-steps-for-sri-lanka-what-is-the-change-we-want-and-can-have-e172dc8667a7",
-      created_at: "2022-03-31T07:26:36.473801+00:00"
+      url: "https://medium.com/@sanjiva.weerawarana/next-steps-for-sri-lanka-what-is-the-change-we-want-and-can-have-e172dc8667a7",
+      created_at: "2022-03-31T07:26:36.473801+00:00",
+      get_title: {
+        title: "Next steps for Sri Lanka — What is the change we want (and can have)? | by Sanjiva Weerawarana | Apr, 2022 | Medium"
+      }
     }
     const wrapper = mount(PostListItem, {
       props: { post },
     })
     const html = wrapper.html();
     expect(html).toContain(new Date(post.created_at.replace(" ", "T")).toLocaleDateString())
-    expect(html).toContain(post.content)
+    expect(html).toContain(post.get_title.title)
   })
 })
