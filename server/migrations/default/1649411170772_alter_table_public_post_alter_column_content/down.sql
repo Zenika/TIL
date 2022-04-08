765cee1ee2ca89f99181240e205f7ef1732187ea
Vincent LANDAIS <vincent.landais@zenika.com>
2022-04-08 11:54:53 +0200

    added remote schema with get_title query which scraps the article page from the url to get the <title> html tag. added as a relation to post

---
+++ server/migrations/default/1649411170772_alter_table_public_post_alter_column_content/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."post" rename column "url" to "content";
---
+++ server/remote-schema/index.js
@@ -0,0 +1,48 @@
+const parseTitle = (body) => {
+  let match = body.match(/<title.*?>(.*)<\/title>/) // regular expression to parse contents of the <title> tag
+  if (!match || typeof match[1] !== 'string')
+    throw new Error('Unable to parse the title tag')
+  return match[1]
+}
+
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const axios = require('axios').default;
+
+const typeDefs = gql`
+  type get_title_res {
+    title: String
+    error: String
+  }
+
+  type Query {
+    get_title(url: String): get_title_res
+  }
+`;
+
+const resolvers = {
+  Query: {
+    get_title: (parent, args, context, info) => {
+      console.log("args", args)
+      try {
+        return axios(args.url)
+          .then(res => res.data)
+          .then(body => parseTitle(body))
+          .then(title => ({ title }))
+          .catch(e => ({ error: e.message }))
+      } catch (e) {
+        return { error: e.message };
+      }
+    },
+  },
+};
+
+// const context = ({ req }) => {
+//   return { headers: req.headers };
+// };
+
+const schema = new ApolloServer({ typeDefs, resolvers/*, context*/ });
+schema.listen({ port: 8000 }).then(({ url }) => {
+  console.log(`schema ready at ${url}`);
+});
+
