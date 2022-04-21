00fde0b2efeda04ad1b9926300c4c20460f9b376
bnyat22 <bnyat.azizsharif@zenika.com>
2022-04-21 13:34:47 +0200

    added comment section on posts

--- client/src/components/PostListItem.vue
+++ client/src/components/PostListItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col-12">
-    <div class="post-list-item">
+  <div class="col-10">
+    <div class="post-list-item ">
       <img
         :src="`https://www.google.com/s2/favicons?sz=256&domain_url=${domainName}`"
         :alt="post.content"
@@ -14,28 +14,50 @@
             v-html="post.get_title.title ? post.get_title.title : post.url"
           ></a>
         </div>
-        <p>
+          <div class="date-comment">
+         <div class="date-username">   
+        <p >
           {{ post.user.username }}
-        </p>
-        <p class="date">
+        </p> 
+     
+     
+             <p class="date">
           {{
             new Date(post.created_at.replace(" ", "T")).toLocaleDateString()
           }}
-        </p>
+         </p>
+         </div>
+          
+           <img @click="openArticle(post.id)"  class="col-2 comment" height="37"   src="../assets/chat-left-text.svg" />
+       </div>
+         </div>  
       </div>
-    </div>
+    
+
   </div>
 </template>
 
 <script setup>
+const { default: router }=require("@/router");
+
 const props = defineProps({
   post: Object,
 });
 
 const domainName = new URL(props.post.url).hostname;
+const openArticle = (id)=>{
+  router.push(`/post/${id}`)
+}
 </script>
 
 <style lang="scss" scoped>
+.date-username {grid-row: 1;grid-column: 1/3;}
+.comment {grid-row: 1; grid-column: 2/3;
+margin-top: 2px;}
+:hover.comment {
+  background-color: #f8f9fa;
+  cursor: pointer;
+}
 :deep(.post-list-item) {
   display: flex;
   align-items: center;
@@ -59,6 +81,15 @@ const domainName = new URL(props.post.url).hostname;
     font-size: 0.8rem;
     font-weight: lighter;
   }
+  .date-comment {
+    display: grid;
+    grid-template-areas: repeat(1, 1fr);
+    grid-template-columns: minmax(80px, auto) 1fr 1fr 1fr 1fr 1fr ;
+    grid-template-rows: minmax(20px, auto);
+    margin-top: 3px;
+    
+    
+  }
 }
 
 @media screen and (max-width: 576px) {
@@ -73,6 +104,8 @@ const domainName = new URL(props.post.url).hostname;
     .post-detail {
       text-align: center;
     }
+
   }
+
 }
 </style>
