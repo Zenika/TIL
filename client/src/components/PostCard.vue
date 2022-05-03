62cd82104221624f3961be26b49c06a4569872bb
Vincent LANDAIS <vincent.landais@zenika.com>
2022-05-03 16:20:05 +0200

    39 (client): PostView splitted, it now uses PostCard

---
+++ client/src/components/PostCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="card-container">
+    <Card class="p-0">
+      <template #content>
+        <div class="grid">
+          <div
+            class="
+              col-11
+              flex flex-column
+              justify-content-center
+              overflow-hidden
+            "
+          >
+            <h3
+              class="
+                font-normal
+                m-0
+                overflow-hidden
+                text-overflow-ellipsis
+                white-space-nowrap
+              "
+            >
+              <a
+                :href="post.post_by_pk.url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {{
+                  post.post_by_pk.get_title.title
+                    ? post.post_by_pk.get_title.title
+                    : post.post_by_pk.url
+                }}
+              </a>
+            </h3>
+            <small class="font-light">
+              {{ post.post_by_pk.user.username }} -
+              {{
+                new Date(
+                  post.post_by_pk.created_at.replace(" ", "T")
+                ).toLocaleDateString()
+              }}
+            </small>
+          </div>
+          <div class="col">
+            <img
+              :src="`https://www.google.com/s2/favicons?sz=256&domain_url=${domainName}`"
+            />
+          </div>
+
+          <div class="col-12 border-top-1 border-200">
+            <p
+              class="pt-0 pb-0"
+              v-html="
+                post.post_by_pk.description
+                  ? post.post_by_pk.description
+                  : '<i>No description</i>'
+              "
+            />
+          </div>
+
+          <div class="col-12">
+            <CommentSection :postId="route.params.id" />
+          </div>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup>
+import { toRefs } from "@vue/reactivity";
+import CommentSection from "../components/CommentSection.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const props = defineProps({
+  post: Object,
+});
+
+const { post } = toRefs(props);
+
+const domainName = new URL(post.value.post_by_pk.url).hostname;
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+}
+
+.card-container >>> .p-card-content {
+  padding: 0;
+}
+</style>
--- client/src/views/PostView.vue
+++ client/src/views/PostView.vue
@@ -13,71 +13,8 @@
         @click="$router.go(-1)"
       />
     </div>
-    <div class="col-6 col-offset-3 card-container">
-      <Card class="p-0">
-        <template #content>
-          <div class="grid">
-            <div
-              class="
-                col-11
-                flex flex-column
-                justify-content-center
-                overflow-hidden
-              "
-            >
-              <h3
-                class="
-                  font-normal
-                  m-0
-                  overflow-hidden
-                  text-overflow-ellipsis
-                  white-space-nowrap
-                "
-              >
-                <a
-                  :href="result.post_by_pk.url"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {{
-                    result.post_by_pk.get_title.title
-                      ? result.post_by_pk.get_title.title
-                      : result.post_by_pk.url
-                  }}
-                </a>
-              </h3>
-              <small class="font-light">
-                {{ result.post_by_pk.user.username }} -
-                {{
-                  new Date(
-                    result.post_by_pk.created_at.replace(" ", "T")
-                  ).toLocaleDateString()
-                }}
-              </small>
-            </div>
-            <div class="col">
-              <img
-                :src="`https://www.google.com/s2/favicons?sz=256&domain_url=${domainName}`"
-              />
-            </div>
-
-            <div class="col-12 border-top-1 border-200">
-              <p
-                class="pt-0 pb-0"
-                v-html="
-                  result.post_by_pk.description
-                    ? result.post_by_pk.description
-                    : '<i>No description</i>'
-                "
-              />
-            </div>
-
-            <div class="col-12">
-              <CommentSection :postId="route.params.id" />
-            </div>
-          </div>
-        </template>
-      </Card>
+    <div class="col-6 col-offset-3 ">
+      <PostCard :post="result"/>
     </div>
   </div>
 </template>
@@ -86,8 +23,8 @@
 import { useRoute } from "vue-router";
 import { useQuery } from "@vue/apollo-composable";
 import gql from "graphql-tag";
-import CommentSection from "../components/CommentSection.vue";
 import NavBar from "@/components/NavBar.vue";
+import PostCard from "@/components/PostCard.vue"
 import { ref, watch } from "@vue/runtime-core";
 
 const route = useRoute();
@@ -115,13 +52,6 @@ const { loading, result, error } = useQuery(
     id: route.params.id,
   }
 );
-
-const domainName = ref(null);
-
-watch(result, (resultValue) => {
-  if (resultValue)
-    domainName.value = new URL(resultValue.post_by_pk.url).hostname;
-});
 </script>
 
 <style scoped>
@@ -131,13 +61,5 @@ watch(result, (resultValue) => {
   top: 50%;
   -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
-}
-
-.card-container >>> .p-card-content {
-  padding: 0;
-}
-
-img {
-  width: 50px;
 }
 </style>
