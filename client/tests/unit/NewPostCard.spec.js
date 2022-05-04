a1952c51e86a91cd9ee35808d70144b06ceffcc0
Vincent LANDAIS <vincent.landais@zenika.com>
2022-05-04 10:10:49 +0200

    39 (client): removed unused unit testing files, added NewPostCard unit tests

---
+++ client/tests/unit/NewPostCard.spec.js
@@ -0,0 +1,46 @@
+import { config, mount } from '@vue/test-utils'
+
+import NewPostCard from "@/components/NewPostCard.vue"
+import Card from 'primevue/card';
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext';
+import Editor from 'primevue/editor';
+
+config.global.components = {
+    'Button': Button,
+    'Card': Card,
+    'Editor': Editor,
+    'InputText': InputText
+  }
+
+describe('NewPostCard.vue', () => {
+    it('empty url', () => {
+        const wrapper = mount(NewPostCard)
+        const submit = wrapper.get('[data-test="submit"]')
+        submit.trigger('click')
+        expect(wrapper.vm.v$.$error).toBe(true)
+        expect(wrapper.emitted('post-click')).toBeUndefined()
+    })
+
+    it('invalid url', async () => {
+        const wrapper = mount(NewPostCard)
+        const submit = wrapper.get('[data-test="submit"]')
+        const input = wrapper.get('[data-test="url"]')
+
+        input.setValue("some bullshit");
+        submit.trigger('click')
+        expect(wrapper.vm.v$.$error).toBe(true)
+        expect(wrapper.emitted('post-click')).toBeUndefined()
+    })
+
+    it('valid url', async () => {
+        const wrapper = mount(NewPostCard)
+        const submit = wrapper.get('[data-test="submit"]')
+        const input = wrapper.get('[data-test="url"]')
+
+        input.setValue("https://www.youtube.com/watch?v=wvZ7ccV8SG8");
+        submit.trigger('click')
+        expect(wrapper.vm.v$.$error).toBe(false)
+        expect(wrapper.emitted('post-click')).toBeTruthy()
+    })
+})
