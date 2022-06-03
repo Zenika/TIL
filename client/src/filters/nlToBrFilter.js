d6e740e6586469e5a154410cfb8ba7cc8ad38f38
Vincent LANDAIS <vincent.landais@zenika.com>
2022-06-03 15:10:01 +0200

    75 (client): added unit tests for escapeHtmlFilter and nlToBrFilter

--- client/src/filters/nlToBrFilter.js
+++ client/src/filters/nlToBrFilter.js
@@ -1,3 +1,3 @@
 import { computed } from "vue";
 
-export const nlToBr = str => computed(() => str.replace(/\n/g, '<br/>'))
+export const nlToBr = str => computed(() => str ? str.replace(/\n/g, '<br/>') : null)
