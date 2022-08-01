72365bb176d0e948519911e6f955fd89eb9dcb1e
Vincent LANDAIS <vincent.landais@epitech.eu>
2022-08-01 11:58:07 +0200

    69 (slack-bot): bot now adds the channel name as a tag to the post and adds OP username to the description

--- slack-bot/src/app.js
+++ slack-bot/src/app.js
@@ -1,5 +1,5 @@
 import { gql } from '@apollo/client/core';
-import { App } from '@slack/bolt';
+import { App, matchEventType } from '@slack/bolt';
 import { apolloClient } from './apollo'
 require('dotenv').config()
 
@@ -15,24 +15,55 @@ const app = new App({
 })();
 
 const mutation = gql`
-    mutation MyMutation($url: String!) {
-        insert_post_one(object: {url: $url, user_id: "test"}) {
+    mutation MyMutation(
+        $url: String!,
+        $description: String,
+        $post_tag_insert_input: [post_tag_insert_input!]!
+        ) {
+        insert_post_one(object: {
+            url: $url,
+            user_id: "test",
+            description: $description
+            post_tags: { data: $post_tag_insert_input }
+        }) {
             add_post_to_rss
         }
     }
 `
 
-app.event('app_mention', async ({ event }) => {
+app.event('app_mention', async ({ event, client }) => {
     console.log(event)
 
     if (event && event.attachments && event.attachments[0] && event.attachments[0].from_url) {
         const url = event.attachments[0].from_url
         console.log(url)
 
+        const { user } = await client.users.info({
+            user: event.user,
+        });
+
+        const { channel } = await client.conversations.info({
+            channel: event.channel
+        })
+
+        const tag = [
+            {
+                tag: {
+                    data: { name: channel.name.toLowerCase() },
+                    on_conflict: {
+                        constraint: "tag_name_key",
+                        update_columns: ["blank"],
+                    },
+                },
+            }
+        ]
+
         apolloClient.mutate({
             mutation,
             variables: {
-                url
+                url,
+                description: `Posted by ${user.name}`,
+                post_tag_insert_input: tag
             }
         }).then(console.log)
             .catch(console.error)
